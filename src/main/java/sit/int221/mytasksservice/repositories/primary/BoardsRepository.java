29d783da006ac3ea63ffb02b73a5e537f902abfa
saksaran <vinzent.vz@gmail.com>
2024-09-16 22:48:55 +0700

    fix: getBoardByOid

--- src/main/java/sit/int221/mytasksservice/repositories/primary/BoardsRepository.java
+++ src/main/java/sit/int221/mytasksservice/repositories/primary/BoardsRepository.java
@@ -3,5 +3,8 @@ package sit.int221.mytasksservice.repositories.primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sit.int221.mytasksservice.models.primary.Boards;
 
+import java.util.List;
+
 public interface BoardsRepository extends JpaRepository<Boards, String> {
+    List<Boards> findBoardsByOid(String oid);
 }
