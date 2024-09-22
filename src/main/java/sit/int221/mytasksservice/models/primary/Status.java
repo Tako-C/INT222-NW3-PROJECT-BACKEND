e168910908e4f457562e1d8a548e9705c0ebb558
saksaran <vinzent.vz@gmail.com>
2024-09-22 13:16:18 +0700

    add: new v3/boards, v3/boards/{boardId}, new visibility public, private add: modify visibility from owner add: new exception 403 fix: owner not display

--- src/main/java/sit/int221/mytasksservice/models/primary/Status.java
+++ src/main/java/sit/int221/mytasksservice/models/primary/Status.java
@@ -1,20 +1,20 @@
-package sit.int221.mytasksservice.models.primary;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
-
-@Entity
-@Getter
-@Setter
-@Table(name="statuses")
-public class Status {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "status_id")
-    private Integer id;
-    @Size(min=1, max=50)
-    private String name;
-    @Size(min=1, max=200)
-    private String description;
-}
+//package sit.int221.mytasksservice.models.primary;
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.Size;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Entity
+//@Getter
+//@Setter
+//@Table(name="statuses")
+//public class Status {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "status_id")
+//    private Integer id;
+//    @Size(min=1, max=50)
+//    private String name;
+//    @Size(min=1, max=200)
+//    private String description;
+//}
--- src/main/java/sit/int221/mytasksservice/repositories/primary/StatusRepository.java
+++ src/main/java/sit/int221/mytasksservice/repositories/primary/StatusRepository.java
@@ -1,8 +1,8 @@
-package sit.int221.mytasksservice.repositories.primary;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sit.int221.mytasksservice.models.primary.Status;
-
-public interface StatusRepository extends JpaRepository<Status, Integer> {
-    Status findByName(String Name);
-}
+//package sit.int221.mytasksservice.repositories.primary;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import sit.int221.mytasksservice.models.primary.Status;
+//
+//public interface StatusRepository extends JpaRepository<Status, Integer> {
+//    Status findByName(String Name);
+//}
